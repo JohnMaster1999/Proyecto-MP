28172c99353625e9d15ffec444d2cb9737169671
PabloACS <100373484+PabloArquelladaCebrian@users.noreply.github.com>
2022-03-21 18:20:59 +0100

    Update Inicio_S.h

--- Inicio_S.h
+++ Inicio_S.h
@@ -1,6 +1,8 @@
 #ifndef Inicio_S
 #define Inicio_S
 
+#include Usuarios.h
+
 //Cabecera: bool exist(Usuarios*).
 //Precondición: recibe la estructura usuarios.
 //Postcondición: devuelve el ID del usuario.
