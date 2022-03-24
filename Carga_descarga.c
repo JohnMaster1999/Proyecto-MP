bffa481b5416f180ec2004b55311c289e8632c01
JohnMaster1999 <John.Master.1999@gmail.com>
2022-03-24 19:33:04 +0100

    Siguiendo la carga de estructuras

--- Carga_descarga.c
+++ Carga_descarga.c
@@ -14,7 +14,20 @@ Carga_datos (Alumnos* al, Materias* mat, Usuarios* us, int* alM, int* matM, int*
 		printf("\nSe ha producido un error en la apertura del fichero: usuarios.txt\n");
 		exit (1);
 	}else{ //USUARIOS.txt abierto exitosamente
-		fgets
+		while(fgets(c, cmax, us)==NULL)
+		{
+			int dato = 1;
+			int i;
+			for (i=0; c[i]!='/0';)
+			{
+				if (c[i]=='-')
+					dato++;
+				switch (dato)
+				{
+
+				}
+			}
+		}
 	}
 
 
