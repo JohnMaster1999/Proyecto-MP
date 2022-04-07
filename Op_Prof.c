da4fa39999ad664ddde818e895d68286f52f17c7
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-07 18:28:34 +0200

    Actualización Juan Repeto

--- Op_Prof.c
+++ Op_Prof.c
@@ -17,129 +17,129 @@ int Opciones()
     printf("\t4.Localidad\n");
     printf("\t5.Curso\n");
     printf("\t6.Grupo\n");
-    printf("\t7.Atrás\n\n");
+    printf("\t7.AtrÃ¡s\n\n");
 
     printf("Indique su opci%cn: ", 162);
     scanf("%d", &option);
+
+    return option;
 }
 
-// ### FUNCIÓN VOLVER_ATRÁS ###
+// ### FUNCIÃ“N VOLVER_ATRÃS ###
 
 Alumnos* Modificar_ficha(int index)
 {
     int op = Opciones();
 
-    //printf("\nIndique el %cndice del alumno: ", 161);
-    //scanf("%d", &index);
-
     switch(op)
     {
         case 1: //IdUs
         {
-            printf("Introduzca el ID nuevo: ");
-            scanf("%d", clase[index].idUs);
-
-            if(clase[index].idUs != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducir el nombre
-            }
+                printf("Introduzca el ID nuevo: ");
+                scanf("%d", clase[index].idUs);
 
+                if(clase[index].idUs != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+
+            }while(clase[index].idUs == NULL);
         }
         break;
 
         case 2: //Nombre
         {
-            printf("Introduzca el Nombre nuevo: ");
-            clase[index].Nomb = gets(); //Lee el nombre, REVISAR
-
-            if(clase[index].Nomb != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducir el nombre
-            }
+                printf("Introduzca el Nombre nuevo: ");
+                clase[index].Nomb = gets(); //Lee el nombre, REVISAR
+
+                if(clase[index].Nomb != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+            }while(clase[index].Nomb == NULL);
         }
         break;
 
-        case 3: //Dirección
+        case 3: //DirecciÃ³n
         {
-            printf("Introduzca la nueva Direcci%cn: ", 162);
-            clase[index].Dir = gets(); //Lee Dir, REVISAR
-
-            if(clase[index].Dir != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducirlo
-            }
+                printf("Introduzca la nueva Direcci%cn: ", 162);
+                clase[index].Dir = gets();
+
+                if(clase[index].Dir != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+            }while(clase[index].Dir == NULL);
         }
         break;
 
         case 4: //Localidad
         {
-            printf("Introduzca la nueva Localidad: ");
-            clase[index].Loc = gets(); //Lee el nombre, REVISAR
-
-            if(clase[index].Loc != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducir el nombre
-            }
+                printf("Introduzca la nueva Localidad: ");
+                clase[index].Loc = gets();
+
+                if(clase[index].Loc != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+            }while(clase[index].Loc == NULL);
+
         }
         break;
 
         case 5: //Curso
         {
-            printf("Introduzca el Curso nuevo: ");
-            clase[index].Curso = gets(); //Lee el nombre, REVISAR
-
-            if(clase[index].Curso != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducir el nombre
-            }
+                printf("Introduzca el Curso nuevo: ");
+                clase[index].Curso = gets(); //Lee el nombre, REVISAR
+
+                if(clase[index].Curso != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+            }while(clase[index].Curso == NULL);
         }
         break;
 
         case 6: //Grupo
         {
-            printf("Introduzca el Grupo nuevo: ");
-            clase[index].Grup = gets(); //Lee el nombre, REVISAR
-
-            if(clase[index].Grup != NULL)
-                printf("Cambio realizado con %cxito.\n", 130);
-            else
+            do
             {
-                printf("Ha ocurrido un error, vuelva a intentarlo.");
-                exit(1); //Vuelve a introducir el nombre
-            }
+                printf("Introduzca el Grupo nuevo: ");
+                clase[index].Grup = gets(); //Lee el nombre, REVISAR
+
+                if(clase[index].Grup != NULL)
+                    printf("Cambio realizado con %cxito.\n", 130);
+                else
+                    printf("Ha ocurrido un error, vuelva a intentarlo.");
+            }while(clase[index].Grup == NULL);
         }
         break;
 
-        case 7: //Volver atrás
+        case 7: //Volver atrÃ¡s
         {
-            // ### Función importante ###
+            // ### FunciÃ³n importante ###
         }
         break;
 
         default:
         {
             printf("Ha ocurrido un error, vuelva a intentarlo.");
-            exit(1);
+
         }
     }
 }
 
-void ficha_alumnos(&clase, i)
+void ficha_alumnos(Alumnos *clase, int  i)
 {
     printf("%d", clase[i].idUs);
     puts(clase[i].Nomb);
@@ -151,17 +151,18 @@ void ficha_alumnos(&clase, i)
     clase = Modificar_ficha(i);
 }
 
-void Calificaciones_alum(&clase, i)
+void Calificaciones_alum(Alumnos *clase, int  i)
 {
 
 }
 
-
-/*Listado de Grupos:
-
-void Menu_Grupos(&clase) ### preguntar al grupo ###
+void Lista_Grupos(Alumnos *clase)
 {
-}
+    //clase.Materias->Calf_->
 
-*/
+    while(num_mat != 0)
+    {
+
+    }
+}
 
