03e49f6c2bea6d7ba88609d7ee6393a877557169
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-19 08:28:02 +0200

    Actualización Juan Repeto

--- Op_alum.h
+++ Op_alum.h
@@ -1,1 +1,14 @@
-//Aquí van los datos y funciones exportables de "OP.ALUM"
+#ifndef OP_ALUMNO_
+#define OP_ALUMNO_
+
+Alumnos *clase;
+int num_alum, num_mat, num_grup, num_calif;
+
+clase = (Alumnos*)malloc(num_alum*sizeof(Alumnos));
+
+void Selec_Alumno(Alumnos *clase, int *a);
+int Lista_alum(Alumnos *clase);
+void ficha_alumnos(Alumnos *clase, int  a);
+void Calificaciones_alum(Alumnos *clase, int  a, int n);
+
+#endif // OP_ALUMNO_
