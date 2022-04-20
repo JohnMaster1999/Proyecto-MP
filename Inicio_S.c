300aee6870ee42e1f959bfa4bb2d40053b726e15
PabloACS <100373484+PabloArquelladaCebrian@users.noreply.github.com>
2022-04-20 22:57:03 +0200

    Update Inicio_S.c

--- Inicio_S.c
+++ Inicio_S.c
@@ -13,48 +13,45 @@ int inicio(Usuarios *user, int n_usuarios)
     int id=0, i=0;
     int n=0;
 
-    printf("            Inicio de Sesion\n-----------------------------------------\n\nUsuario:");
+    printf("______________________________________________________\n\t\tInicio de Sesi%cn\n______________________________________________________\n\nUsuario:", 162);
 
     usu = (char*)malloc(6*sizeof(char));
     pass = (char*)malloc(9*sizeof(char));
-    
+
     do{
-    fgets(usu, 6, stdin);
+    fgets(usu, 6, stdin); //Lee un usuario
     fflush(stdin);
     usu[6]='\0';
         do{
-            if(strcmp(usu, user[i].NomU)==0 && strlen(usu)==strlen(user[i].NomU))
+            if(strcmp(usu, user[i].NomU)==0 && strlen(usu)==strlen(user[i].NomU)) //Busca el usuario para ver si existe en nuestra estructura de usuarios
             {
-                printf("    Usuario encontrado, introduzca la contraseña.\n");
+                printf("\tUsuario encontrado, introduzca la contrase%ca.\n", 164); //Si el usuario coincide pide la contraseña.
                 n=1;
             }
-            else
+            else  //Si el usuario no coincide, lo compara con el siguiente de la lista.
                 i++;
-        }while(i < n_usuarios || n==0);
+        }while(i < n_usuarios && n==0);
 
         if(i == n_usuarios)
-                printf("    Usuario no encontrado, vuelva a introducir el usuario.\nUsuario:");
+                printf("\tUsuario no encontrado, vuelva a introducir el usuario.\nUsuario:"); //Si el usuario no existe, vuelve a padir uno nuevo.
 
     }while(n==0);
-    
-    fgets(pass, 9, stdin);
-    fflush(stdin);
-    usu[9]='\0';
-    
+
     while(n==1)
     {
-        printf("Contraseña:");
-        
-        fgets(pass, 9, stdin);
+        printf("\nContraseña:");
+
+        fgets(pass, 9, stdin); //Lee una contraseña.
         fflush(stdin);
         usu[9]='\0';
-            
-        if(strcmp(pass, user[i].pass)==0 && strlen(pass)==strlen(user[i].pass)){
-            printf("    Contraseña correcta.\n\n\nIniciando sesión...");
+
+        if(strcmp(pass, user[i].pass)==0 && strlen(pass)==strlen(user[i].pass)) //Comprueba si la contraseña del usuario actual es correcta.
+            {
+            printf("\t\tContraseña correcta.\n\n\nIniciando sesión..."); //Si es correcta inicia sesión.
             n=0;
             }
         else
-            printf("    Contraseña incorrecta, vuelve a introducirla.");
+            printf("\t\tContraseña incorrecta, vuelve a introducirla."); //Si es incorrecta vuelve a pedir la contraseña.
     }
-    return i;
+    return i; //Devuelve la posición del usuario en el vector de usuarios.
 }
