b2cf25c7318e698a0872c54f19461036f4343804
PabloACS <100373484+PabloArquelladaCebrian@users.noreply.github.com>
2022-04-20 23:06:22 +0200

    Update Menus.h

--- Menus.h
+++ Menus.h
@@ -10,6 +10,6 @@
 //Cabecera: void menu_prin(Usuarios*, Alumnos*, Materias*, Fecha*, int)
 //Precondición: recibe los punteros de las estructuras que necesita y la posicion en el vector estructura del usuario.
 //Postcondición: dependiendo de si es un usuario administrador o profesor, lo envia a su respectivo menú.
-void menu_prin(Usuarios*, Alumnos*, Materias*, Fecha*, int);
+void menu_prin(Usuarios*, Alumnos*, Materias*, Fecha*, int, int*, int*, int*, int*, int*);
 #endif // Menus
 
