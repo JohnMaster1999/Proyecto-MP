2b818577c3b70ea1863e381d53834f6f2d11b620
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-20 18:20:21 +0200

    Actualización Juan Repeto

--- Op_alum.h
+++ Op_alum.h
@@ -2,13 +2,24 @@
 #define OP_ALUMNO_
 
 Alumnos *clase;
-int num_alum, num_mat, num_grup, num_calif;
+int num_alum, num_mat, num_grup, num_calif, salir = 0;
 
 clase = (Alumnos*)malloc(num_alum*sizeof(Alumnos));
 
+//Precondición: Recibe como parámetros la estructura de alumnos y una variable por referencia.
+//Postcondición: Se modifica el valor de la variable en un paso por referencia.
 void Selec_Alumno(Alumnos *clase, int *a);
+
+//Precondición: Recibe como parámetros el vector de la estructura de Alumnos.
+//Postcondición: Imprime una lista de los alumnos (elementos del vector de estructura Alumnos).
 int Lista_alum(Alumnos *clase);
-void ficha_alumnos(Alumnos *clase, int  a);
+
+//Precondición: Recibe como parámetros la estructura de alumnos y dos variables.
+//Postcondición: Imprime los datos del alumno y permite modificarlos.
+void ficha_alumnos(Alumnos *clase, int  a, int n);
+
+//Precondición: Recibe como parámetros la estructura de alumnos y dos variables.
+//Postcondición: Imprime una lista de los alumnos (y una vez seleccionado) modifica, borra o crea las calificaciones del alumno.
 void Calificaciones_alum(Alumnos *clase, int  a, int n);
 
 #endif // OP_ALUMNO_
