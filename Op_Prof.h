8b10fb7374ac7f7104b63aebabe4e4acf7c69985
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-20 18:18:56 +0200

    Actualización Juan Repeto

--- Op_Prof.h
+++ Op_Prof.h
@@ -6,6 +6,12 @@ int num_alum, num_mat, num_grup;
 
 clase = (Alumnos*)malloc(num_alum*sizeof(Alumnos));
 
+// Precondición: Recibe un entero que sirve como índice para un alumno seleccionado.
+// Postcondición: Permite modificar los distintos campos de la Estructura Alumno.
 void Modificar_ficha(int index);
+
+// Precondición: Recibe como parámetros el vector de la estructura de alumnos y un entero que indica el numero de grupos
+// Postcondición: Permite listar alumnos o cambiar de grupo.
+void Menu_Grupo(Alumnos *clase, int n);
 
 #endif // OP_PROFESOR_
