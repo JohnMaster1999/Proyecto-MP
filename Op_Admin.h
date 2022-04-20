b88157c0e02407f2b6ffde69d3112f7881e34748
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-20 19:41:08 +0200

    Actualización Juan Repeto

--- Op_Admin.h
+++ Op_Admin.h
@@ -3,7 +3,7 @@
 
 //Precondición: Recibe como parámetros los vectores de estructuras de los módulos de estructuras.
 //Postcondición: Realiza la operación que se indique (Dar de alta, Dar de baja, Modificar, Listar) con las estructuras que se le pasan.
-void Menu_admin(Usuarios *prof, Alumnos *clase, Materias *mat, Horarios *hor,  int *num_alum, int *num_mat, int *num_prof, int *num_hor);
+void Menu_admin(Usuarios *prof, Alumnos *clase, Materias *mat, Horarios *hor,  int *num_alum, int *num_mat, int *num_prof, int *num_hor, int salir)
 //### Cuando se llame a esta función hay que meterla en un do-while ###
 
 #endif // OP_ADMIN_
