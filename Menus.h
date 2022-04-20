925707115272fff75c36f19d5ca0d0adb205d25c
PabloACS <100373484+PabloArquelladaCebrian@users.noreply.github.com>
2022-04-20 18:19:20 +0200

    Update Menus.h

--- Menus.h
+++ Menus.h
@@ -1,12 +1,12 @@
 #ifndef Menus
 #define Menus
 
-#include Op_Admin.h
-#include Op_Prof.h
-#include Op_alum.h
-#include Op_mat.h
-#include Op_usuario.h
-#include Inicio_S.h
+#include "Op_Admin.h"
+#include "Op_Prof.h"
+#include "Op_alum.h"
+#include "Op_mat.h"
+#include "Op_usuario.h"
+#include "Inicio_S.h"
 
 //Cabecera: void menu_prin(Usuarios*, Alumnos*, Materias*, Fecha*, int)
 //Precondición: recibe los punteros de las estructuras que necesita y la posicion en el vector estructura del usuario.
