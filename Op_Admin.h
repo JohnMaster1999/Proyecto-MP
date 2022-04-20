555c2e4c8fcc9580c85153c28470265a950ac990
juanrd27 <100841852+juanrd27@users.noreply.github.com>
2022-04-20 18:15:20 +0200

    Actualización Juan Repeto

--- Op_Admin.h
+++ Op_Admin.h
@@ -1,16 +1,20 @@
 #ifndef OP_ADMIN_
 #define OP_ADMIN_
 
-Alumnos *clase;
-Usuarios *prof;
-Materias *mat;
-Fecha *hor;
-
-int num_prof, num_alum, num_mat, num_hor;
+Alumnos *clase; // Vector de la estructura Alumnos.
+Usuarios *prof; // Vector de la estructura Usuarios.
+Materias *mat; // Vector de la estructura Materias.
+Fecha *hor; // Vector de la estructura Fecha.
+int num_prof, num_alum, num_mat, num_hor; //Variables que almacenan el tamaño de las distintas estructuras.
 
 prof = (Usuarios*)malloc(num_prof*sizeof(Usuarios));
 clase = (Alumnos*)malloc(num_alum*sizeof(Alumnos));
 mat = (Materias*)malloc(num_mat*sizeof(Materias));
 clase = (Fecha*)malloc(num_hor*sizeof(Fecha));
 
+//Precondición: Recibe como parámetros los vectores de estructuras de los módulos de estructuras.
+//Postcondición: Realiza la operación que se indique (Dar de alta, Dar de baja, Modificar, Listar) con las estructuras que se le pasan.
+void Menu_admin(Usuarios *prof, Alumnos *clase, Materias *mat, Fechas *hor); // (Menús)
+
 #endif // OP_ADMIN_
+
